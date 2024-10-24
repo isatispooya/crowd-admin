7cd30406ac3d256a21c64032296e8819c7f96bd2
ZahraAmini <112477210+ZahraAmini1112@users.noreply.github.com>
2024-10-24 12:58:07 +0330

    ...

--- src/app.jsx
+++ src/app.jsx
@@ -1,26 +1,22 @@
 /* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable perfectionist/sort-imports */
 
 import { createTheme } from '@mui/material/styles';
 import { useScrollToTop } from 'src/hooks/use-scroll-to-top';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Router from 'src/routes/sections';
 import ThemeProvider from 'src/theme';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { prefixer } from 'stylis';
 import rtlPlugin from 'stylis-plugin-rtl';
+
 import createCache from '@emotion/cache';
 import { CacheProvider } from '@emotion/react';
 import 'src/global.css';
 import 'react-toastify/dist/ReactToastify.css';
-import 'react-datepicker/dist/react-datepicker.css'; 
+import 'react-datepicker/dist/react-datepicker.css';
 import 'tabulator-tables/dist/css/tabulator.min.css';
 import 'react-tabulator/lib/styles.css';
 import 'react-tabulator/lib/css/tabulator_bootstrap4.min.css';
-
-
-
-
 
 const theme = createTheme({
   palette: {
@@ -30,7 +26,6 @@ const theme = createTheme({
   },
 });
 
-
 const cacheRtl = createCache({
   key: 'muirtl',
   stylisPlugins: [prefixer, rtlPlugin],
@@ -39,6 +34,13 @@ const cacheRtl = createCache({
 export default function App() {
   useScrollToTop();
   const [queryClient] = useState(() => new QueryClient());
+
+  useEffect(() => {
+    if (window.self !== window.top) {
+      window.top.location = window.self.location;
+    }
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <CacheProvider value={cacheRtl}>
