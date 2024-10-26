67dba67498bcf2d6fb8a7636057c91ed5d0e3357
ZahraAmini <112477210+ZahraAmini1112@users.noreply.github.com>
2024-10-26 09:48:21 +0330

    ..

--- src/layouts/dashboard/service/logout.js
+++ src/layouts/dashboard/service/logout.js
@@ -1,18 +1,20 @@
 import api from 'src/api/apiClient';
 import { getCookie } from 'src/api/cookie';
 
-const accessApi = getCookie('accessApi');
-
 export const LogOut = async () => {
-  const url = `/api/logout/admin/`;
-  const response = await api.post(
-    url,
-    {
-      headers: {
-        Authorization: `Bearer ${accessApi}`,
-        'Content-Type': 'application/json',
-      },
-    }
-  );
+  const url = `/api/log/out/`;
+  const accessApi = getCookie('accessApi');
+
+  if (!accessApi) {
+    return null;
+  }
+
+  const response = await api.post(url, null, {
+    headers: {
+      Authorization: `Bearer ${accessApi}`,
+      'Content-Type': 'application/json',
+    },
+  });
+
   return response.data;
 };
