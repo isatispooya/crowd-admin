bbb104937f8a98c658decdecbc4095002a0bc2c4
zahraAm <zahraamini.yazd20@gmai.com>
2024-12-15 15:34:50 +0330

    Refactor usePostInvestor hook: Removed authorization header handling by eliminating access token retrieval from cookies, streamlining API request structure for improved clarity and maintainability.

--- src/module/paln/feature/participant/hooks/usePostInvestor.js
+++ src/module/paln/feature/participant/hooks/usePostInvestor.js
@@ -1,19 +1,12 @@
 import { useMutation } from '@tanstack/react-query';
 
 import api from 'src/api/apiClient';
-import { getCookie } from 'src/api/cookie';
 
 const usePostInvestor = (traceCode) => {
-  const accessApi = getCookie('accessApi');
   const sendInvestor = async (id) => {
     const response = await api.post(
       `api/payment/inquiry/admin/${traceCode}/`,
       { id },
-      {
-        headers: {
-          Authorization: `Bearer ${accessApi}`,
-        },
-      }
     );
     return response.data;
   };
