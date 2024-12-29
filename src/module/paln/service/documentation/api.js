dd4d98388e563d3453f9fa26787e42afe8cdfc38
zahraAm <zahraamini.yazd20@gmai.com>
2024-12-29 15:32:31 +0330

    Enhance API interactions in documentation service: Added authorization headers with Bearer token for GetDocument, PostDocument, and DeleteDocument functions to improve security. Removed unnecessary whitespace in usePostDocumentation file for cleaner code.

--- src/module/paln/service/documentation/api.js
+++ src/module/paln/service/documentation/api.js
@@ -1,8 +1,14 @@
 import api from 'src/api/apiClient';
+import { getCookie } from 'src/api/cookie';
 
+const accessApi = getCookie('accessApi');
 
 export const GetDocument = async (trace_code) => {
   const response = await api.get(`/api/documentation/${trace_code}/`, {
+    headers: {
+      Authorization: `Bearer ${accessApi}`,
+      'Content-Type': 'application/json',
+    },
   });
   return response.data;
 };
@@ -17,6 +23,10 @@ export const PostDocument = async (trace_code, postData) => {
     formData.append('title', postData.title);
   }
   const response = await api.post(`/api/documentation/${trace_code}/`, formData, {
+    headers: {
+      Authorization: `Bearer ${accessApi}`,
+      'Content-Type': 'multipart/form-data',
+    },
   });
 
   return response.data;
@@ -24,6 +34,10 @@ export const PostDocument = async (trace_code, postData) => {
 
 export const DeleteDocument = async (docId) => {
   const response = await api.delete(`/api/documentation/${docId}/`, {
+    headers: {
+      Authorization: `Bearer ${accessApi}`,
+      'Content-Type': 'application/json',
+    },
   });
   return response.data;
 };
