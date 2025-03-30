68a815b8d4e28336b95a80fa9231347400b9afcf
نام کاربری شما < >
2025-03-30 09:22:47 +0330

    Add Page6 and Page7 to AgencyContract, enhancing contract details and layout for improved readability. Update index.js to include new pages in exports and adjust PAGES array accordingly.

--- src/module/Create_receiveRrequests/AgencyContract/feature/warTreaty/index.js
+++ src/module/Create_receiveRrequests/AgencyContract/feature/warTreaty/index.js
@@ -3,8 +3,12 @@ import Page2 from './page2';
 import Page3 from './page3';
 import Page4 from './page4';
 import Page5 from './page5';
+import Page6 from './page6';
+import Page7 from './page7';
+import Page8 from './page8';
+import Page9 from './page9';
 
-export { Page1, Page2, Page3, Page4, Page5 };
+export { Page1, Page2, Page3, Page4, Page5, Page6, Page7, Page8, Page9 };
 
-export const PAGES = [Page1, Page2, Page3, Page4, Page5];
+export const PAGES = [Page1, Page2, Page3, Page4, Page5, Page6, Page7, Page8, Page9];
 export const TOTAL_PAGES = PAGES.length;
