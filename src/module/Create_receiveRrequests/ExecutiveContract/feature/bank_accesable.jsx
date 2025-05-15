d7553a05be2bc84915256ebd7e52b2dc474752a5
نام کاربری شما < >
2025-05-15 14:48:10 +0330

    Add payment information display in BankAccesable component
    Enhanced the BankAccesable component by adding a new section that displays payment details, including the payment amount, payment status, reference number, and tracking ID. This addition aims to improve user experience by providing clear and accessible payment information.

--- src/module/Create_receiveRrequests/ExecutiveContract/feature/bank_accesable.jsx
+++ src/module/Create_receiveRrequests/ExecutiveContract/feature/bank_accesable.jsx
@@ -58,6 +58,32 @@ const BankAccesable = ({ allData }) => {
           labelPlacement="start"
         />
       </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 2,
+          boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.1)',
+          padding: 2,
+          borderRadius: '10px',
+          backgroundColor: '#ffffff',
+          mt: 2,
+        }}
+      >
+        <Typography variant="body1" sx={{ fontWeight: 'bold' }}>
+          مبلغ پرداخت: {allData?.amount_of_payment}
+        </Typography>
+        <Typography variant="body1" sx={{ fontWeight: 'bold' }}>
+          وضعیت پرداخت:{' '}
+          {allData?.code_status_payment === 'success' ? 'پرداخت شده' : ' در انتظار پرداخت'}
+        </Typography>
+        <Typography variant="body1" sx={{ fontWeight: 'bold' }}>
+          شماره پیگیری پرداخت: {allData?.reference_number_payment}
+        </Typography>
+        <Typography variant="body1" sx={{ fontWeight: 'bold' }}>
+          شماره پیگیری پرداخت: {allData?.track_id_payment}
+        </Typography>
+      </Box>
     </Box>
   );
 };
