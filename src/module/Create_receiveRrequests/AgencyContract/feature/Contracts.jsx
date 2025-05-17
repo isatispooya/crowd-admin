bf912f2af9e18811c5af0cfc738851f15dfa28a6
mhmdalisadat <150137653+mhmdalisadat@users.noreply.github.com>
2025-05-17 10:40:40 +0330

    Enhance Contracts component by adding payment status checks and disabling links accordingly
    Updated the Contracts component to include a check for payment status and agency contract details, disabling relevant links when conditions are not met. This change improves user experience by preventing navigation to incomplete contract sections.

--- src/module/Create_receiveRrequests/AgencyContract/feature/Contracts.jsx
+++ src/module/Create_receiveRrequests/AgencyContract/feature/Contracts.jsx
@@ -13,21 +13,30 @@ const Contracts = ({ data }) => {
     }
   }, [data, initializeStore]);
 
-  const links = [
-    { id: 1, title: 'قرارداد عاملیت', path: `/agency/${data?.uuid}` },
-    { id: 3, title: 'نامه بانکی', path: `/bankLetter/?uuid=${data?.uuid}` },
-    { id: 4, title: 'قرارداد اجرایی', path: `/executiveContract/${data?.uuid}` },
-    { id: 5, title: 'قرارداد مشارکت', path: `/WarTreaty/${data?.uuid}` },
-  ];
+  console.log(data, 1212342434);
 
-  const isButtonsDisabled =
-    !agencyContract.bank_letter_number || !agencyContract.agency_agreement_date;
+  const checkPayment =
+    data.code_status_payment !== 'success' ||
+    !agencyContract.bank_letter_number ||
+    !agencyContract.agency_agreement_date;
+
+  const links = [
+    { id: 1, title: 'قرارداد عاملیت', path: `/agency/${data?.uuid}`, disabled: checkPayment },
+    { id: 3, title: 'نامه بانکی', path: `/bankLetter/?uuid=${data?.uuid}`, disabled: checkPayment },
+    {
+      id: 4,
+      title: 'قرارداد اجرایی',
+      path: `/executiveContract/${data?.uuid}`,
+      disabled: checkPayment,
+    },
+    { id: 5, title: 'قرارداد مشارکت', path: `/WarTreaty/${data?.uuid}`, disabled: checkPayment },
+  ];
 
   return (
     <Container maxWidth="md" dir="rtl">
       {links.map((link) => (
         <Button
-          key={link.id} ر
+          key={link.id}
           component={Link}
           to={link.path}
           target="_blank"
@@ -35,7 +44,7 @@ const Contracts = ({ data }) => {
           fullWidth
           variant="outlined"
           sx={{ mb: 1, display: 'flex', justifyContent: 'space-between' }}
-          disabled={link.id === 3 ? isButtonsDisabled : false}
+          disabled={link.disabled}
         >
           {link.title}
         </Button>
