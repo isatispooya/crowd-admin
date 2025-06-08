d7732d70e62ebd162a80310d4fa2fe31ce3a466d
mhmdalisadat <150137653+mhmdalisadat@users.noreply.github.com>
2025-06-08 20:42:37 +0330

    Refactor AppendixContract pages by removing unused components and enhancing Page1 layout
    Deleted Page2 and Page3 components from the AppendixContract module as they are no longer needed. Updated Page1 to improve its layout, added prop types for better type checking, and included additional null checks for safer data handling. Enhanced the rendering of contract details to ensure clarity and consistency.

--- src/module/Create_receiveRrequests/AppendixContract/pages/index.js
+++ src/module/Create_receiveRrequests/AppendixContract/pages/index.js
@@ -1,8 +1,6 @@
 import Page1 from './page1';
-import Page2 from './page2';
-import Page3 from './page3';
 
-export { Page1, Page2, Page3 };
+export { Page1 };
 
-export const PAGES_APPENDIX = [Page1, Page2, Page3];
+export const PAGES_APPENDIX = [Page1];
 export const TOTAL_PAGES_APPENDIX = PAGES_APPENDIX.length;
--- src/module/Create_receiveRrequests/AppendixContract/pages/page3.jsx
+++
@@ -1,9 +0,0 @@
-const Page3 = () => {
-  return (
-    <div>
-      <h1>Page3</h1>
-    </div>
-  );
-};
-
-export default Page3;
